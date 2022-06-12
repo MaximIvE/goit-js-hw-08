041acb881421e038481123f39f391b3d265ae71b
Maksym Ivaniuk <ivanukmaxim@gmail.com>
2022-06-13 01:19:12 +0300

    add task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import Player from '@vimeo/player';
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+var _ = require('lodash');
+
+// багато часу шукав назву обробника події при перезавантаженні форми, для addEventListener, потім знайшов 'onload', але воно в мене не запрацювало. 
+// Тому через let для змінної.
+// я просто подумав, що весь js наново завантажується після перезавантаження сторінки, а локалсторідж ні. а якщо там записів ще не було, то повернеться null
+let time;
+
+player.on("timeupdate", _.throttle((e) => {
+        const timeJsonRec = JSON.stringify(e.seconds);
+        localStorage.setItem("videoplayer-current-time", timeJsonRec);
+    }, 1000)
+);
+
+const timeJsonRead = localStorage.getItem("videoplayer-current-time");
+time = timeJsonRead ? JSON.parse(timeJsonRead) : 0;
+player.setCurrentTime(time);
+
+
+
+
 
