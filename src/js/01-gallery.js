c985725eb6a75882be87631b38698c64cd45f193
Maksym Ivaniuk <ivanukmaxim@gmail.com>
2022-06-12 19:59:14 +0300

    add task 01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,38 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+//console.log(); це я поставив щоб швидко копіпастити
+
+// console.log(galleryItems);
+// console.log(SimpleLightbox);
+
+// ►►► Використовуємо код з дз 7 №2 та перероблюємо (а він чомусь і так нормально запрацював ☺ ♪♫♪ )
+
+const galleryEl = document.querySelector(".gallery");
+
+function createEl(src, href, alt){
+    return `<a class="gallery__item" href="${href}">
+    <img class="gallery__image" src="${src}" alt="${alt}" />
+  </a>`
+
+};
+
+const galleryStringEl = galleryItems.reduce((contanier, galleryItem) => {
+  const {preview, original, description} = galleryItem;
+  
+  contanier = contanier + createEl(preview, original, description);
+  return contanier;
+}, "");
+
+galleryEl.insertAdjacentHTML("beforeend", galleryStringEl);
+
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: "alt",
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
