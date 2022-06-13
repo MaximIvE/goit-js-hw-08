ff71ef908181e5feb836ceba25cbd7d3b0bd4e37
Maksym Ivaniuk <ivanukmaxim@gmail.com>
2022-06-13 22:21:53 +0300

    add task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,27 @@
+const form = document.querySelector('.feedback-form');
 
+let dataJson = localStorage.getItem("feedback-form-state");
+const inputData = dataJson ? JSON.parse(dataJson) : {email: "", message: ""};
+
+var _ = require('lodash');
+
+if (inputData.email) { form[0].value = inputData.email};
+if (inputData.message) { form[1].value = inputData.message};
+
+form.addEventListener('input', _.throttle((e)=>{
+    element = e.target;
+
+    //записуємо дані в об'єкт
+    inputData[element.name] = element.value;
+
+    //переводимо inputData в Json та записуємо в Local Storage
+    const dataJson = JSON.stringify(inputData);
+    localStorage.setItem("feedback-form-state", dataJson);
+}, 500));
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    console.log(inputData);
+    localStorage.removeItem("feedback-form-state");
+    form.reset();
+});
