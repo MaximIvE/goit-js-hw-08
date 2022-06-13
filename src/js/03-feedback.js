da0c60437fdc01403e76977566bd6589ac3c4144
Maksym Ivaniuk <ivanukmaxim@gmail.com>
2022-06-13 23:13:49 +0300

    adds lodash to task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,12 +3,12 @@ const form = document.querySelector('.feedback-form');
 let dataJson = localStorage.getItem("feedback-form-state");
 const inputData = dataJson ? JSON.parse(dataJson) : {email: "", message: ""};
 
-// const _ = require('lodash');
+const _ = require('lodash');
 
 if (inputData.email) { form[0].value = inputData.email};
 if (inputData.message) { form[1].value = inputData.message};
 
-form.addEventListener('input', (e)=>{
+form.addEventListener('input', _.throttle((e)=>{
     const element = e.target;
 
     //записуємо дані в об'єкт
@@ -17,7 +17,7 @@ form.addEventListener('input', (e)=>{
     //переводимо inputData в Json та записуємо в Local Storage
     const dataJson = JSON.stringify(inputData);
     localStorage.setItem("feedback-form-state", dataJson);
-});
+}, 500));
 
 form.addEventListener('submit', (e)=>{
     e.preventDefault();
